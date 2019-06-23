ddc6962fd47441c7c20096a5b3aa4256d5242bd8
kartik Asodariya <kartikasodariya17@gmail.com>
2019-06-24 00:40:46 +0530

    customer Specific product extension

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -4,11 +4,25 @@ namespace Nexgento\Customerspecific\Controller\Index;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
+	protected $_session;
+
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Customer\Model\Session $session)
+	{
+		$this->_session = $session;
+		return parent::__construct($context);
+	}
+
     public function execute()
     {
+    	if ($this->_session->isLoggedIn()) {
+	        $this->_view->loadLayout();
+	        $this->_view->getLayout()->initMessages();
+	        $this->_view->renderLayout();
+	    } else {
+	    	return $this->resultRedirectFactory->create()->setPath('customer/account/', ['_current' => true]);
 
-        $this->_view->loadLayout();
-        $this->_view->getLayout()->initMessages();
-        $this->_view->renderLayout();
+	    }
     }
 }
